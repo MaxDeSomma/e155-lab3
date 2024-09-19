d7bb3ff929fae6a2c4810a995b2bb994e1bb1c09
MaxDeSomma <133174685+MaxDeSomma@users.noreply.github.com>
2024-09-19 16:06:38 -0700

    Update ledController.sv

--- fpga/src/ledController.sv
+++ fpga/src/ledController.sv
@@ -1,6 +1,14 @@
+// ledController.sv
+// Max De Somma
+// mdesomma@g.hmc.edu
+// 9/19/24
+
+// this module similar to last week determines when each seven segment display should be on
+// however it also accoutns for debouncing by waiting for a few clock cycles after a button is pressed before the led changes.
+// additionally it slides the numbers so the new number is on the right
 module ledController(
     input logic clk, reset,
-	input logic [24:0] bounceCounter,
+    input logic [24:0] bounceCounter,
     input logic [3:0] s,
     input logic en,
     output logic [6:0] seg,
@@ -13,26 +21,29 @@ module ledController(
 		
 	// use a flip flop to flip toggle at around 240Hz
 	always_ff @(posedge clk) begin
-            if(reset) begin
-                s1 <= 4'b0;
-                s2 <= 4'b0;
-            end
-            if(en) begin
-				canSwitch <= 1;
-            end
-			if(canSwitch && bounceCounter >= 10) begin
-				s2 <= s1;
-                s1 <= s;
-				canSwitch <= 0;
-			end
+        	if(reset) begin
+                	s1 <= 4'b0;
+                	s2 <= 4'b0;
+            	end
+		// hold the enable
+            	if(en) begin
+			canSwitch <= 1;
+            	end
+		// if enable is on and its waited for the debouncing then slide the numbers over and reset the enable
+	    	if(canSwitch && bounceCounter >= 10) begin
+			s2 <= s1;
+                	s1 <= s;
+			canSwitch <= 0;
+		end
 
-			counter <= counter + 1;
-			if(counter == 100) begin
-					counter <= 0;
-					toggle <= ~toggle;
-					// depending on which display shoudl be on assign s to input s1 or s2
-					if(toggle == 0) ledNum = s1;
-					else ledNum = s2;
+		counter <= counter + 1;
+		// timing to determine when the left and rigth seven segment should be on
+		if(counter == 100) begin
+				counter <= 0;
+				toggle <= ~toggle;
+				// depending on which display shoudl be on assign s to input s1 or s2
+				if(toggle == 0) ledNum = s1;
+				else ledNum = s2;
 			end
 		end
 	
